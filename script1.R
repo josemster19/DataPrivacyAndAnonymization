ea9ab60c4c456066a9482946b0aead0621ae6605
Jose Antonio Cegarra <josemster19@gmail.com>
2019-04-11 16:12:41 +0200

    Se habia hecho mal el commit anterior, esto es el script actualizado

--- script1.R
+++ script1.R
@@ -2,16 +2,29 @@
 #Autors: José Antonio Cegarra Alonso 1461305
 #Autors: Juan Manuel Vallecillos Calzado 1401596
 
-#instalar el paquete
+#instal·lar el paquet
 install.packages("sdcMicro")
 
 #Carrega de les llibreries
 library(sdcMicro)
 
-#Carregar les dades 
+#Carregar les dades (Compte amb el path, en windows es diferent)
 dades <- read.csv(file = "/data/salaris.csv", header=TRUE, sep=",", colClasses = c("CP"="character"))
 
 #Mostra de les dades
 colnames(dades)
 head(dades)
 summary(dades)
+
+# Visualitzar els 10 primers valors
+dades[1:10, "Edat"]
+
+# Calcul mean i sd
+print(paste("Atribut 'Edat': mean value =", mean(dades[, "Edat"]), "and SD =", sd(dades[, "Edat"]), sep=" "))
+
+# Visualitzacio dels valors (ordenats)
+plot(sort(dades[, "Edat"]), type="p", col="red", xlab="Registres", ylab="Valor", main="Edat")
+
+# Creem un subset de dades eliminant els identificadors (DNI i nombre de la SS)
+d_subset = d[,c(1,3,5)]
+
