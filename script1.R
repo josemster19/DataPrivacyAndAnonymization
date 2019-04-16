b948c8ab73a5146f2075c9ff03153ca9d1abb871
Jose Antonio Cegarra <josemster19@gmail.com>
2019-04-16 12:38:17 +0200

    He hecho la primera parte del ejercicio 2 ya se identifican los atributos de manera unica pero NO ESTA hecha la combinación de 2 columnas a ver si hay una combinación única

--- script1.R
+++ script1.R
@@ -2,14 +2,17 @@
 #Autors: José Antonio Cegarra Alonso 1461305
 #Autors: Juan Manuel Vallecillos Calzado 1401596
 
-#instal·lar el paquet
+#instal·lació de paquets
 install.packages("sdcMicro")
+install.packages("dplyr")
 
 #Carrega de les llibreries
 library(sdcMicro)
+library(plyr)
+library(dplyr)
 
 #Carregar les dades (Compte amb el path, en windows es diferent)
-dades <- read.csv(file = "/data/salaris.csv", header=TRUE, sep=",", colClasses = c("CP"="character"))
+dades <- read.csv(file = "~/UNIVERSIDAD/3r Curso/2Semestre/GIS/Practica/Practica2/GIS/data/salaris.csv", header=TRUE, sep=",", colClasses = c("CP"="character"))
 
 #Mostra de les dades
 colnames(dades)
@@ -26,5 +29,22 @@ print(paste("Atribut 'Edat': mean value =", mean(dades[, "Edat"]), "and SD =", s
 plot(sort(dades[, "Edat"]), type="p", col="red", xlab="Registres", ylab="Valor", main="Edat")
 
 # Creem un subset de dades eliminant els identificadors (DNI i nombre de la SS)
-d_subset = d[,c(1,3,5)]
+dades_subset = dades[,c(3,4,5)]
 
+#Creem un subset amb la frequencia del CP
+dades_CP_freq = plyr::count(dades_subset, 'CP')
+
+#Creem un subset amb la frequencia de l'edat
+dades_Edat_freq = plyr::count(dades_subset, 'Edat')
+
+#Creem un subset amb la frequencia del salari
+dades_Salari_freq = plyr::count(dades_subset, 'Salari')
+
+#Creem un subset amb els valors únics del CP
+dades_unique_CP = dplyr::filter(dades_CP_freq, freq == "1")
+
+#Creem un subset amb els valors únics de l'Edat
+dades_unique_Edat = dplyr::filter(dades_Edat_freq, freq == "1")
+
+#Creem un subset amb els valors únics del Salari
+dades_unique_Salari = dplyr::filter(dades_Salari_freq, freq == "1")
