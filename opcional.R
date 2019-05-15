29361093eed87ee754be806767f2ee01bf7d78f8
Jose Antonio Cegarra <josemster19@gmail.com>
2019-05-15 18:09:42 +0200

    Trabajando en el apartado b del opcional. De momento he trabajado con CP y Ocupacion. He eliminado los identificadores.

---
+++ opcional.R
@@ -0,0 +1,85 @@
+# Script de la part opcional de la practica 
+# Autors: José Antonio Cegarra Alonso 1461305
+# Autors: Juan Manuel Vallecillos Calzado 1401596
+
+# Instal·lació de paquets
+install.packages("sdcMicro")
+
+# Carrega de les llibreries
+library(sdcMicro)
+
+# Carregar les dades (Compte amb el path, en windows es diferent)
+dades <- read.csv(file = "~/UNIVERSIDAD/3r Curso/2Semestre/GIS/Practica/Practica2/GIS/data/hipoteca.csv", header=TRUE, sep=",", colClasses = c("CP"="character"))
+
+# Apartat b
+# Primer eliminem els identificadors
+dades_subset = dades[,c(2,4,5,6,7,8,9,10)]
+
+# Generalitzem el CP
+
+ProvinciaCP <- function(CP) {
+  CP = substr(CP,1,2)
+  padding = "000"
+  CP = paste(CP,padding,sep="")
+  return (CP)
+}
+
+# Creem un nou data frame i generalitzem el CP
+CPGeneralitzat <- dades_subset
+CPGeneralitzat$CP=ProvinciaCP(dades_subset$CP)
+
+
+# Clasifiquem els treballs en branques
+GeneralitzarOcupacio <- function(Ocupacio) {
+  counter = 1
+  value <- c()
+  for(Oindv in Ocupacio){
+    if(Oindv == levels(CPGeneralitzat$Ocupacio)[1]){
+      value = append(value,"Informàtic")
+    }
+    if(Oindv == levels(CPGeneralitzat$Ocupacio)[2]){
+      value = append(value,"Informàtic")
+    }
+    if(Oindv == levels(CPGeneralitzat$Ocupacio)[3]){
+      value = append(value,"Tècnic")
+    }
+    if(Oindv == levels(CPGeneralitzat$Ocupacio)[4]){
+      value = append(value,"Tècnic")
+    }
+    if(Oindv == levels(CPGeneralitzat$Ocupacio)[5]){
+      value = append(value,"Medicina")
+    }
+    if(Oindv == levels(CPGeneralitzat$Ocupacio)[6]){
+      value = append(value,"Medicina")
+    }
+    if(Oindv == levels(CPGeneralitzat$Ocupacio)[7]){
+      value = append(value,"Informàtic")
+    }
+    if(Oindv == levels(CPGeneralitzat$Ocupacio)[8]){
+      value = append(value,"Tècnic")
+    }
+    if(Oindv == levels(CPGeneralitzat$Ocupacio)[9]){
+      value = append(value,"Mecànic")
+    }
+    if(Oindv == levels(CPGeneralitzat$Ocupacio)[10]){
+      value = append(value,"Mecànic")
+    }
+    if(Oindv == levels(CPGeneralitzat$Ocupacio)[11]){
+      value = append(value,"Mecànic")
+    }
+    if(Oindv == levels(CPGeneralitzat$Ocupacio)[12]){
+      value = append(value,"Medicina")
+    }
+    if(Oindv == levels(CPGeneralitzat$Ocupacio)[13]){
+      value = append(value,"Informàtic")
+    }
+    if(Oindv == levels(CPGeneralitzat$Ocupacio)[14]){
+      value = append(value,"Tècnic")
+    }
+    counter= counter + 1
+  }
+  return (value)
+}
+
+CPGeneralitzat$Ocupacio = GeneralitzarOcupacio(CPGeneralitzat$Ocupacio)
+
